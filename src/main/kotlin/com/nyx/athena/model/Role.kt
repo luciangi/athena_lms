366e2be304808ddb7923b0677a3b94be867676b5
Lucian Gabriel Ilie <luciangabrielilie@gmail.com>
2018-06-04 14:21:05 +0300

    Role unique authority

--- src/main/kotlin/com/nyx/athena/model/Role.kt
+++ src/main/kotlin/com/nyx/athena/model/Role.kt
@@ -4,13 +4,10 @@ import org.hibernate.annotations.GenericGenerator
 import org.hibernate.annotations.Type
 import org.hibernate.annotations.TypeDef
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
-@Table
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["authority"])])
 @TypeDef(name = "uuid-custom", typeClass = UUIDCustomType::class)
 class Role(val authority: String = Authority.ROLE_STUDENT.name) {
     enum class Authority { ROLE_ADMIN, ROLE_TUTOR, ROLE_STUDENT }
--- src/test/groovy/com/nyx/athena/repository/RoleRepositoryTest.groovy
+++ src/test/groovy/com/nyx/athena/repository/RoleRepositoryTest.groovy
@@ -4,20 +4,21 @@ import com.nyx.athena.BaseIntegrationTest
 import com.nyx.athena.model.Role
 import org.springframework.beans.factory.annotation.Autowired
 
+import static com.nyx.athena.model.Role.Authority.ROLE_STUDENT
+
 class RoleRepositoryTest extends BaseIntegrationTest {
     @Autowired
     RoleRepository roleRepository
 
     def "test create Role"() {
         given:
-        Role adminRole = new Role()
-        adminRole.id = UUID.randomUUID()
+        Role adminRole = new Role(ROLE_STUDENT.name())
 
         when:
         roleRepository.save(adminRole)
 
         then:
-        roleRepository.findOne(adminRole.id) == adminRole
+        roleRepository.findByAuthority(ROLE_STUDENT.name()) == adminRole
 
         cleanup:
         roleRepository.deleteAll()
